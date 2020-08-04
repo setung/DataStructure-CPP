19b0766d32b23f821985639d0879882577109d19
setung <32754444+setung@users.noreply.github.com>
2020-08-04 17:05:50 +0900

    우선순위 큐 구현

---
+++ Priority_Queue/Priority_Queue/Priority_Queue.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include "MaxHeap.h"
+
+/*
+     우선순위 큐
+          배열, 링크드리스트로 구현이 가능하지만 
+          보통 완전이진트리인 힙으로 구현을 함
+
+     힙
+      중복 허용함
+      쵀대 힙 : 부모키가 자식키보다 크거나 같음
+      최소 힙 : 반대
+
+
+*/
+
+
+void heapSort(int a[], int n) {
+     MaxHeap h;
+     for (int i = 0; i < n; i++)
+          h.insert(a[i]);
+
+     for (int i = n - 1; i >= 0; i--)
+          a[i] = h.remove().getKey();
+}
+
+int main()
+{
+     MaxHeap heap;
+
+     heap.insert(10);
+     heap.insert(5);
+     heap.insert(30);
+     heap.insert(7);
+     heap.insert(9);
+     heap.insert(3);
+     heap.insert(10);
+     heap.display();
+
+     heap.remove();
+     heap.display();
+     heap.remove();
+     heap.display();
+     heap.remove();
+     heap.display();
+
+     int data[10];
+     cout << "힙정렬 전 : ";
+     for (int i = 0; i < 10; i++) {
+          data[i] = rand() % 100;
+          cout << data[i] << ' ';
+     }
+     cout << '\n';
+
+     heapSort(data, 10);
+
+     cout << "힙정렬 후 : ";
+     for (int i = 0; i < 10; i++) {
+          cout << data[i] << ' ';
+     }
+     cout << '\n';
+}
+
--- Recursion/미로 탐색/미로 탐색.cpp
+++ Recursion/미로 탐색/미로 탐색.cpp
@@ -15,7 +15,7 @@ char map[MAZE_SIZE][MAZE_SIZE] = {
 struct Location2D {
      int row;
      int col;
-
+     
      Location2D(int r = 0, int c = 0) { row = r; col = c; }
 
      bool isNeighbor(Location2D& p) {
